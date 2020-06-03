72ab005e5947c1e7c8c881dac84b78d3bbae980e
JesusVMayor <jesus@comunitea.com>
2020-06-03 13:50:24 +0200

    [FIX]custom_prestashop: Evitamos crear registros de categorías de partners.

--- project-addons/custom_prestashop/models/res_partner/importer.py
+++ project-addons/custom_prestashop/models/res_partner/importer.py
@@ -10,3 +10,10 @@ class PartnerImportMapper(Component):
     @mapping
     def groups(self, record):
         return {'category_id': [(6, 0, [self.env.ref('custom_prestashop.prestashop_tag').id])]}
+
+
+class ResPartnerImporter(Component):
+    _inherit = "prestashop.res.partner.importer"
+
+    def _import_dependencies(self):
+        return
