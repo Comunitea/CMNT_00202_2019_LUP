5248b2ad29271df2bdc7e8b96e03e19d2f03fc46
Santi <santi@comunitea.com>
2020-08-19 11:09:04 +0200

    [IMP] cusotm_lupeon. Ajuste par aenvio de facturas con  tipo de persona correcto

--- project-addons/custom_lupeon/__manifest__.py
+++ project-addons/custom_lupeon/__manifest__.py
@@ -10,11 +10,12 @@
     "license": "AGPL-3",
     "depends": ["product", "sale_stock", "delivery", "account_payment", 
                 "sale_margin", "sale_order_margin_percent", "stock_barcode",
-                "stock", "account", "custom_prestashop"],
+                "stock", "account", "custom_prestashop", "l10n_es_facturae"],
     "data": [
         'views/partner_view.xml',
         'views/sale_view.xml',
         'views/invoice_view.xml',
         'views/stock_view.xml',
-        'views/payment_mode_view.xml'],
+        'views/payment_mode_view.xml',
+        'views/report_facturae.xml'],
 }
