7f8701d0633b42c0d0d34dedbe3090a602dc231e
javierjcf <javierjcf@gmail.com>
2020-09-25 14:03:03 +0200

    [FIX]cost_sheet_lupeon: Enlace a costes desde workorder

---
+++ project-addons/cost_sheet_lupeon/models/mrp_workorder.py
@@ -0,0 +1,12 @@
+# © 2020 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models, fields, api, _
+
+
+class MrpWorkorder(models.Model):
+
+    _inherit = "mrp.workorder"
+
+    sheet_id = fields.Many2one(
+        'cost.sheet', 'Cost Sheet', related='production_id.sheet_id')
