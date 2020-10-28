48f5841007d87ee151e1221a8c1ff4a169103023
javierjcf <javierjcf@gmail.com>
2020-10-28 16:41:37 +0100

    [FIX]scrip: Limpiado de lupeon

--- scripts/clean_lupeon_models.sql
+++ scripts/clean_lupeon_models.sql
@@ -1,8 +1,16 @@
 -- Limpiado de lupeon
-delete from sale_order where company_id = 1 and id!=1520;
+delete from sale_order where company_id = 1 and id;
 delete from stock_picking where company_id = 1;
 delete from stock_move where company_id = 1;
+
+
 delete from stock_move_line where picking_id is null;
+
+delete from stock_move_line where id in (
+    select sml.id, from stock_move_line sml
+    inner join stock_move sm on sml.move_id = sm.id where sml.picking_id is null and sm.company_id = 1);
+
+
 delete from project_project pp where company_id = 1;
 delete from project_task where company_id = 1;
 delete from change_production_qty;
@@ -14,6 +22,6 @@ delete from cost_sheet where sale_line_id is null;
 delete from purchase_order where company_id = 1;
 delete from group_production;
 delete from mrp_routing where created_on_fly is true;
-
+delete from mrp_bom where company_id = 1;
 -- otras
-update product_template set tracking='lot' where is_material is True;
+-- update product_template set tracking='lot' where is_material is True;
