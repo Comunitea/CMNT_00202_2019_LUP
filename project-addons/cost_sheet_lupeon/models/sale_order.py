6c5c0b0ff2fe3dc93e9dee430c7b53f25ec75131
javierjcf <javierjcf@gmail.com>
2020-12-14 11:41:01 +0100

    [FIX]cost_sheet_lupeon: Descripcion de pedido obligatoria, notas no

--- project-addons/cost_sheet_lupeon/models/sale_order.py
+++ project-addons/cost_sheet_lupeon/models/sale_order.py
@@ -37,7 +37,7 @@ class SaleOrder(models.Model):
 
     purchase_ids = fields.One2many(
         'purchase.order', 'dest_sale_id', 'Purchases')
-    name2 = fields.Char('Descripción pedido')
+    name2 = fields.Char('Descripción pedido', required=True)
     cost_sheet_sale = fields.Boolean(related='company_id.cost_sheet_sale')
 
     @api.multi
