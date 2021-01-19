a5272f72d6968396623c4b8241db4d1a30bf5945
javierjcf <javierjcf@gmail.com>
2021-01-19 10:29:38 +0100

    [FIX]cost_sheet_lupeon: Editable en estado diseño

--- project-addons/cost_sheet_lupeon/models/group_cost_sheet.py
+++ project-addons/cost_sheet_lupeon/models/group_cost_sheet.py
@@ -61,7 +61,7 @@ class GroupCostSheet(models.Model):
                 pu = group.line_pvp / group.sale_line_id.product_uom_qty
 
             if group.sale_id and group.sale_id.state in (
-                    'draft', 'sent'):
+                    'draft', 'sent', 'design'):
                 group.sale_line_id.write({'price_unit': pu})
 
     @api.depends('sheet_ids', 'assembly_ids')
