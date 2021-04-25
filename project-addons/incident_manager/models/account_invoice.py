c6ae5fed2bc03ed4e69937fb3fcd92381383a867
Santi Argüeso <santi@comunitea.com>
2021-04-25 14:55:15 +0200

    [IMP] incident_manager

--- project-addons/incident_manager/models/account_invoice.py
+++ project-addons/incident_manager/models/account_invoice.py
@@ -2,6 +2,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 
-class StockPicking(models.Model):
-    _inherit = ["stock.picking", "model.incident.base"]
-    _name = "stock.picking"
+class AccountInvoice(models.Model):
+    _inherit = ["account.invoice", "model.incident.base"]
+    _name = "account.invoice"
