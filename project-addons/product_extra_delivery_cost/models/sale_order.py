4b65c7d4b5808358509da49591845e1c0de4f93f
Santi Argüeso <santi@comunitea.com>
2021-05-25 07:59:52 +0200

    [FIX] product_extra_delivery_cost: Corregido precio al ñadir la linea

--- project-addons/product_extra_delivery_cost/models/sale_order.py
+++ project-addons/product_extra_delivery_cost/models/sale_order.py
@@ -7,17 +7,34 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
-    def get_delivery_price(self):
-        super().get_delivery_price()
-        for order in self.filtered(lambda o: o.state in ('draft', 'sent') and len(o.order_line) > 0):
-            if order.delivery_rating_success:
-                so_lines = order.order_line.filtered(lambda x: x.product_id.delivery_extra_cost)
-                if so_lines:
-                    extra_price = sum(so_lines.mapped('product_id').mapped('delivery_extra_cost'))
-                    order.delivery_price += extra_price
-                    if not order.delivery_message:
-                        order.delivery_message = "\n Se ha aplicado un coste extra de {} ".format(extra_price)
-                    else:
-                        order.delivery_message += "\n Se ha aplicado un coste extra de {}".format(extra_price)
+    # def get_delivery_price(self):
+    #     super().get_delivery_price()
+    #     for order in self.filtered(lambda o: o.state in ('draft', 'sent') and len(o.order_line) > 0):
+    #         if order.delivery_rating_success:
+    #             so_lines = order.order_line.filtered(lambda x: x.product_id.delivery_extra_cost)
+    #             if so_lines:
+    #                 extra_price = sum(so_lines.mapped('product_id').mapped('delivery_extra_cost'))
+    #                 order.delivery_price += extra_price
+    #                 if not order.delivery_message:
+    #                     order.delivery_message = "\n Se ha aplicado un coste extra de {} ".format(extra_price)
+    #                 else:
+    #                     order.delivery_message += "\n Se ha aplicado un coste extra de {}".format(extra_price)
 
-                           
+
+class DeliveryCarrier(models.Model):
+    _inherit = "delivery.carrier"
+
+    def rate_shipment(self, order):
+        res = super().rate_shipment(order)
+        self.ensure_one()
+        
+        if res['success']:
+            so_lines = order.order_line.filtered(lambda x: x.product_id.delivery_extra_cost)
+            if so_lines:
+                extra_price = sum(so_lines.mapped('product_id').mapped('delivery_extra_cost'))
+                res['price'] += extra_price
+                if not res['warning_message']:
+                    res['warning_message'] = "\n Se ha aplicado un coste extra de {} ".format(extra_price)
+                else:
+                    res['warning_message'] += "\n Se ha aplicado un coste extra de {}".format(extra_price)
+        return res
