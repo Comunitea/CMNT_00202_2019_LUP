632bc6d663072b04b1d04910982cf4247f487869
vicentecom <vicente@comunitea.com>
2021-09-21 16:34:53 +0200

    [IMP] custom_stock_barcode: Eliminados datos de posición

--- project-addons/custom_stock_barcode/static/src/js/client_action/lines_widget.js
+++ project-addons/custom_stock_barcode/static/src/js/client_action/lines_widget.js
@@ -19,30 +19,18 @@ odoo.define('custom_stock_barcode.LinesWidget', function (require) {
            var $pageSummary = $(QWeb.render('stock_barcode_summary_template', {
                // location
                locationName: this.page.location_name,
-               locationPosx: this.page.location_posx,
-               locationPosy: this.page.location_posy,
-               locationPosz: this.page.location_posz,
                // location dest
                locationDestName: this.page.location_dest_name,
-               locationDestPosx: this.page.location_dest_posx,
-               locationDestPosy: this.page.location_dest_posy,
-               locationDestPosz: this.page.location_dest_posz,
                // parent
                locationParent: this.page.location_parent,
                locationParentFormat: this.page.location_parent_format,
                // gparent
                locationGParent: this.page.location_gparent,
-               locationGParentPosx: this.page.location_gparent_posx,
-               locationGParentPosy: this.page.location_gparent_posy,
-               locationGParentPosz: this.page.location_gparent_posz,
                // parent dest
                locationDestParent: this.page.location_dest_parent,
                locationDestParentFormat: this.page.location_dest_parent_format,
                // gparent dest
                locationDestGParent: this.page.location_dest_gparent,
-               locationDestGParentPosx: this.page.location_dest_gparent_posx,
-               locationDestGParentPosy: this.page.location_dest_gparent_posy,
-               locationDestGParentPosz: this.page.location_dest_gparent_posz,
                nbPages: this.nbPages,
                pageIndex: this.pageIndex + 1,
                mode: this.mode,
