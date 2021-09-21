10283811810c82a29566c8dba8d62819fa03d965
vicentecom <vicente@comunitea.com>
2021-09-21 11:36:20 +0200

    [IMP] custom_stock_barcode: Si la ubicación padre es box se muestran tanto la ubicación padre como la ubicación del box

--- project-addons/custom_stock_barcode/models/stock_picking.py
+++ project-addons/custom_stock_barcode/models/stock_picking.py
@@ -20,15 +20,24 @@ class StockPicking(models.Model):
                         move_line_id['location_id']['posz'] = mvl_location_id['posz']
 
                         mov_loc_id_parent = self.env['stock.location'].browse(mvl_location_id['location_id'][0])
-                        move_line_id['location_id']['location_id'] = mov_loc_id_parent.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz',])[0]
+                        move_line_id['location_id']['location_id'] = mov_loc_id_parent.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz', 'location_id'])[0]
+
+                        if mov_loc_id_parent and mov_loc_id_parent.location_id:
+                            mov_loc_id_gparent = mov_loc_id_parent.location_id
+                            move_line_id['location_id']['location_id']['location_id'] = mov_loc_id_gparent.read(['id', 'display_name', 'posx', 'posy', 'posz',])[0]
 
                     mvl_location_dest_id = self.env['stock.location'].browse(move_line_id['location_dest_id']['id']).read(['location_id','posx', 'posy', 'posz',])[0]
                     if mvl_location_dest_id and mvl_location_dest_id['location_id'] and mvl_location_dest_id['location_id'][0]:
                         move_line_id['location_dest_id']['posx'] = mvl_location_dest_id['posx']
                         move_line_id['location_dest_id']['posy'] = mvl_location_dest_id['posy']
                         move_line_id['location_dest_id']['posz'] = mvl_location_dest_id['posz']
+
                         mov_loc_id_parent_dest = self.env['stock.location'].browse(mvl_location_dest_id['location_id'][0])
-                        move_line_id['location_dest_id']['location_id'] = mov_loc_id_parent_dest.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz',])[0]
+                        move_line_id['location_dest_id']['location_id'] = mov_loc_id_parent_dest.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz', 'location_id'])[0]
+
+                        if mov_loc_id_parent_dest and mov_loc_id_parent_dest.location_id:
+                            mov_loc_id_gparent_dest = mov_loc_id_parent_dest.location_id
+                            move_line_id['location_dest_id']['location_id']['location_id'] = mov_loc_id_gparent_dest.read(['id', 'display_name', 'posx', 'posy', 'posz',])[0]
 
             # Adding parent location_id/location_dest_id in pickings
             if pick['location_id'] and pick['location_id']['id']:
@@ -39,7 +48,11 @@ class StockPicking(models.Model):
                     pick['location_id']['posz'] = parent_location_id['posz']
 
                     plocation_id = self.env['stock.location'].browse(parent_location_id['location_id'][0])
-                    pick['location_id']['location_id'] = plocation_id.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz'])[0]
+                    pick['location_id']['location_id'] = plocation_id.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz', 'location_id'])[0]
+
+                    if plocation_id and plocation_id.location_id:
+                        gplocation_id = plocation_id.location_id
+                        pick['location_id']['location_id']['location_id'] = gplocation_id.read(['id', 'display_name', 'posx', 'posy', 'posz',])[0]
             
             if pick['location_dest_id'] and pick['location_dest_id']['id']:
                 parent_location_dest_id = self.env['stock.location'].browse(pick['location_dest_id']['id']).read(['location_id','posx', 'posy', 'posz',])[0]
@@ -49,5 +62,9 @@ class StockPicking(models.Model):
                     pick['location_dest_id']['posz'] = parent_location_dest_id['posz']
 
                     plocation_dest_id = self.env['stock.location'].browse(parent_location_dest_id['location_id'][0])
-                    pick['location_dest_id']['location_id'] = plocation_dest_id.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz'])[0]
+                    pick['location_dest_id']['location_id'] = plocation_dest_id.read(['id', 'display_name', 'loc_format', 'posx', 'posy', 'posz', 'location_id'])[0]
+
+                    if plocation_dest_id and plocation_id.location_id:
+                        gplocation_dest_id = plocation_id.location_id
+                        pick['location_dest_id']['location_id']['location_id'] = gplocation_dest_id.read(['id', 'display_name', 'posx', 'posy', 'posz',])[0]
         return res
--- project-addons/custom_stock_barcode/static/src/js/client_action/picking_client_action.js
+++ project-addons/custom_stock_barcode/static/src/js/client_action/picking_client_action.js
@@ -13,19 +13,33 @@ odoo.define('custom_stock_barcode.picking_client_action', function (require) {
         _getPageFields: function () {
             var res = this._super();
             res.push(
+                // parent
                 ['location_parent', 'location_id.location_id.display_name'],
                 ['location_parent_format', 'location_id.location_id.loc_format'],
                 ['location_parent_posx', 'location_id.location_id.posx'],
                 ['location_parent_posy', 'location_id.location_id.posy'],
                 ['location_parent_posz', 'location_id.location_id.posz'],
+                // gparent
+                ['location_gparent', 'location_id.location_id.location_id.display_name'],
+                ['location_gparent_posx', 'location_id.location_id.location_id.posx'],
+                ['location_gparent_posy', 'location_id.location_id.location_id.posy'],
+                ['location_gparent_posz', 'location_id.location_id.location_id.posz'],
+                // parent dest
                 ['location_dest_parent', 'location_dest_id.location_id.display_name'],
-                ['location_dest_parent_format', 'location_id.location_id.loc_format'],
-                ['location_dest_parent_posx', 'location_id.location_id.posx'],
-                ['location_dest_parent_posy', 'location_id.location_id.posy'],
-                ['location_dest_parent_posz', 'location_id.location_id.posz'],
+                ['location_dest_parent_format', 'location_dest_id.location_id.loc_format'],
+                ['location_dest_parent_posx', 'location_dest_id.location_id.posx'],
+                ['location_dest_parent_posy', 'location_dest_id.location_id.posy'],
+                ['location_dest_parent_posz', 'location_dest_id.location_id.posz'],
+                // gparent dest
+                ['location_dest_gparent', 'location_dest_id.location_id.location_id.display_name'],
+                ['location_dest_gparent_posx', 'location_dest_id.location_id.location_id.posx'],
+                ['location_dest_gparent_posy', 'location_dest_id.location_id.location_id.posy'],
+                ['location_dest_gparent_posz', 'location_dest_id.location_id.location_id.posz'],
+                // location
                 ['location_posx', 'location_id.posx'],
                 ['location_posy', 'location_id.posy'],
                 ['location_posz', 'location_id.posz'],
+                // location dest
                 ['location_dest_posx', 'location_dest_id.posx'],
                 ['location_dest_posy', 'location_dest_id.posy'],
                 ['location_dest_posz', 'location_dest_id.posz'],
